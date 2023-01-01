073a7366900d86c3d1ab0ccb506fb4f9d93d6c23
ericjonesdev <ericjones2004@yahoo.com>
2023-01-01 20:47:22 +0000

    Plot the bar chart section

--- run.py
+++ run.py
@@ -67,3 +67,12 @@ df_grouped = df[mask].groupby(by=['Rating']).count()[['Age']]
 df_grouped = df_grouped.rename(columns={'Age': 'Votes'})
 df_grouped = df_grouped.reset_index()
 
+# --- PLOT BAR CHART
+bar_chart = px.bar(df_grouped,
+                   x='Rating',
+                   y='Votes',
+                   text='Votes',
+                   color_discrete_sequence = ['#9965D5']*len(df_grouped),
+                   template= 'plotly_white')
+st.plotly_chart(bar_chart)
+
